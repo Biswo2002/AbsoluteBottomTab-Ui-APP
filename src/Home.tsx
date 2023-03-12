133f86ec8247d560150e40b3fb6e96ffce056dbc
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-03-12 13:03:18 +0530

    ABSOLUTE BOTTOM TAB

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import BottomNavigator from './BottomNavigator/BottomTab'
+
+const Home = () => {
+    return (
+        <BottomNavigator />
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
